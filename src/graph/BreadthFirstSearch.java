86ba49b727e60701c384930459494b0e6ac77ad2
Kirill Sokolov <sokolov.k.o@yandex.ru>
2019-03-21 21:48:05 +0300

    add BreadthFirstSearch

--- src/graph/BreadthFirstSearch.java
+++ src/graph/BreadthFirstSearch.java
@@ -1,9 +1,10 @@
 package graph;
 
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
+import java.util.Queue;
 
-public class DepthFirstSearch {
+public class BreadthFirstSearch {
     public static void main(String[] args) {
         int[][] graph = {
                 {0, 1, 0, 0, 1, 0, 0, 0},
@@ -14,23 +15,27 @@ public class DepthFirstSearch {
                 {0, 0, 0, 0, 0, 0, 0, 0},
                 {1, 0, 0, 0, 1, 0, 0, 0},
                 {0, 1, 0, 0, 0, 0, 0, 0}
-    };
+        };
         List<Node> list = Utils.toGraphList(graph);
-        Stack<Node> stack = new Stack<>();
+        Queue<Node> stack = new LinkedList<>();
         visit(7, list, stack);
         Utils.printVisited(list);
     }
 
-    public static void visit(int index, List<Node> graph, Stack<Node> stack) {
-        Node node = graph.get(index);
+    private static void visit(int item, List<Node> list, Queue<Node> queue) {
+        Node node = list.get(item);
         node.setVisited(true);
-        for(int i = 0; i < node.getOut().size(); i++){
-            Node next = node.getOut().get(i);
-            stack.push(next);
-            visit(next.getIndex(), graph, stack);
+        for (int i = 0; i < node.getOut().size(); i++) {
+            Node tmpNode = node.getOut().get(i);
+            if (!tmpNode.isVisited()) {
+                tmpNode.setVisited(true);
+                queue.add(tmpNode);
+            }
         }
-        if (!stack.isEmpty()){
-            visit(stack.pop().getIndex(), graph, stack);
+
+        if (!queue.isEmpty()) {
+            Node next = queue.remove();
+            visit(next.getIndex(), list, queue);
         }
     }
 }
